db348e9524291d97ceb05feb4404ff7821e84bb8
Simonas Viliunas <vilisimo@gmail.com>
2017-11-20 17:08:20 +0200

    Add temporary datasource for testing purposes

--- presentation/src/main/java/vault/resource/BookResource.java
+++ presentation/src/main/java/vault/resource/BookResource.java
@@ -8,19 +8,22 @@ import vault.model.Book;
 import javax.validation.Valid;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Path("/recommendations/books")
 public class BookResource {
 
     private static final Logger LOG = LoggerFactory.getLogger(Book.class);
 
+    private static final Map<String, Book> tempDatasource = new HashMap<>();
+
     @POST
     @Timed
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response postSuggestion(@Valid Book book, @Context UriInfo uriInfo) {
+        tempDatasource.put(book.id(), book);
+
         UriBuilder pathBuilder = uriInfo.getAbsolutePathBuilder();
         pathBuilder.path(book.id());
 
@@ -35,9 +38,7 @@ public class BookResource {
     @Timed
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAll() {
-        List<Book> books = new ArrayList<>();
-        books.add(new Book("First suggestion", "Because"));
-        books.add(new Book("Second suggestion", "I said so"));
+        List<Book> books = new ArrayList<>(tempDatasource.values());
 
         return Response.ok(books).build();
     }
@@ -46,16 +47,16 @@ public class BookResource {
     @Timed
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getOne(@PathParam("id") String id) {
-        Book book = new Book("Just a placeholder", "Placeholder");
-
-        return Response.ok(book).build();
+    public Optional<Book> getOne(@PathParam("id") String id) {
+        return Optional.ofNullable(tempDatasource.get(id));
     }
 
     @PUT
     @Timed
     @Path("/{id}")
-    public Response update(@PathParam("id") String id) {
+    public Response update(@PathParam("id") String id, @Valid Book book) {
+        tempDatasource.put(book.id(), book);
+
         return Response.noContent().build();
     }
 
@@ -63,6 +64,12 @@ public class BookResource {
     @Timed
     @Path("/{id}")
     public Response delete(@PathParam("id") String id) {
-        return Response.accepted().build();
+        Book book = tempDatasource.remove(id);
+
+        if (book != null) {
+            return Response.noContent().build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
     }
 }
