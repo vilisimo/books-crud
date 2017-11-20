7746506d22dd6cad7a5a2a13fdd8158967fabbf7
vilisimo <vilisimo@gmail.com>
2017-11-20 18:56:45 +0200

    Fix resource test

--- presentation/src/test/groovy/vault/resource/BookResourceTest.groovy
+++ presentation/src/test/groovy/vault/resource/BookResourceTest.groovy
@@ -15,9 +15,15 @@ class BookResourceTest extends Specification {
             .addResource(new BookResource())
             .build()
 
-    def "Posting a suggestion returns 201"() {
-        when: "suggestion is posted"
-            def book = "{\"description\":\"test\",\"reason\":\"historic\"}"
+    def "Posting a book returns 201"() {
+        when: "book is posted"
+            def book = "{" +
+                    "\"author\": \"J.R.R. Tolkien\"," +
+                    "\"title\": \"The Hobbit\"," +
+                    "\"description\": \"A book about a hobbit's adventures\"," +
+                    "\"reason\": \"It is a light-hearted, easy to read and yet immersive book\"," +
+                    "\"amazon\": \"https://www.amazon.com/Hobbit-J-R-Tolkien/dp/054792822X\"," +
+                    "\"goodreads\": \"https://www.goodreads.com/book/show/5907.The_Hobbit\"}"
             def response = resources.client().target("/recommendations/books")
                     .request(MediaType.APPLICATION_JSON_TYPE)
                     .post(Entity.json(book))
