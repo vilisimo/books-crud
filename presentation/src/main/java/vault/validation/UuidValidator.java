f29220c4fce4bcc5583244dc22a525aaa4735813
vilisimo <vilisimo@gmail.com>
2017-11-21 20:10:28 +0200

    Assign regex a self-explanatory variable name

--- presentation/src/main/java/vault/validation/UuidValidator.java
+++ presentation/src/main/java/vault/validation/UuidValidator.java
@@ -8,8 +8,8 @@ import java.util.regex.Pattern;
 
 public class UuidValidator implements ConstraintValidator<UUID, String> {
 
-    private static final Pattern pattern =
-            Pattern.compile("^[0-9a-f]{8}-?[0-9a-f]{4}-?[1-5][0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$");
+    private static final String UUID_PATTERN = "^[0-9a-f]{8}-?[0-9a-f]{4}-?[1-5][0-9a-f]{3}-?[89ab][0-9a-f]{3}-?[0-9a-f]{12}$";
+    private static final Pattern pattern = Pattern.compile(UUID_PATTERN);
 
     @Override
     public void initialize(UUID constraintAnnotation) {}
