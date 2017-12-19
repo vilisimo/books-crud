5f5fb81c08755920c836c9d05787cb63d8141b5d
Simonas Viliunas <vilisimo@gmail.com>
2017-12-19 17:35:39 +0200

    Add temp poc config for liquibase

--- persistence/src/main/java/vault/PersistenceApp.java
+++ persistence/src/main/java/vault/PersistenceApp.java
@@ -3,7 +3,10 @@ package vault;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import io.dropwizard.Application;
+import io.dropwizard.db.DataSourceFactory;
 import io.dropwizard.jdbi.DBIFactory;
+import io.dropwizard.migrations.MigrationsBundle;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 import org.skife.jdbi.v2.DBI;
 import vault.jdbi.BookDAO;
@@ -14,6 +17,21 @@ public class PersistenceApp extends Application<MainConfiguration> {
 
     public static void main(String[] args) throws Exception {
         new PersistenceApp().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<MainConfiguration> bootstrap) {
+        bootstrap.addBundle(new MigrationsBundle<MainConfiguration>() {
+            @Override
+            public DataSourceFactory getDataSourceFactory(MainConfiguration configuration) {
+                return configuration.getDataSourceFactory();
+            }
+
+            @Override
+            public String getMigrationsFileName() {
+                return "migrations.yaml";
+            }
+        });
     }
 
     @Override
@@ -29,6 +47,6 @@ public class PersistenceApp extends Application<MainConfiguration> {
         final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "hsqldb");
 
         final BookDAO dao = jdbi.onDemand(BookDAO.class);
-        dao.createTable(); // TODO: remove POC functionality later
+        // dao.createTable(); // TODO: remove POC functionality later
     }
 }
